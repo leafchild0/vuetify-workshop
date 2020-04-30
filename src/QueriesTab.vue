e600405106ff5238d5e7b01ebe7c6e2c2e00130c
Victor Malyshev <vmalyshev0@gmail.com>
2020-04-30 15:26:10 +0300

    Header component

--- src/QueriesTab.vue
+++ src/QueriesTab.vue
@@ -1,24 +1,30 @@
 <template>
   <div id='queries-tab'>
+    <tab-header></tab-header>
   </div>
 </template>
 
 <script>
 
+	import TabHeader from './components/Header.vue';
+
 	export default {
 		name: 'queries-tab',
 		components: {
+			TabHeader
 		}
 	};
 </script>
 
 <style lang="scss">
-#app {
+#queries-tab {
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: rgb(226, 226, 226);
+  margin: 10px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header-tab">
+	  <span class="header-tab__status">Status</span>
+	  <span class="header-tab__template">Template</span>
+	  <span class="header-tab__physician">Physician</span>
+	  <span class="header-tab__date">Send Date</span>
+	  <span class="header-tab__impact">Expected Impact</span>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: 'tab-header'
+	};
+</script>
+
+<style scoped lang="scss">
+	.header-tab {
+		display: flex;
+		justify-content: space-between;
+		margin: 0 20px;
+		font-size: 14px;
+		font-weight: 700;
+		height: 50px;
+		align-items: center;
+		color: grey;
+
+		&__status {
+			width: 160px;
+		}
+
+		&__template {
+			flex: 1;
+			justify-content: flex-start;
+		}
+
+		&__physician {
+			width: 160px;
+		}
+
+		&__date {
+			width: 100px;
+		}
+
+		&__impact {
+			width: 140px;
+		}
+	}
+</style>
